f159b38cea415e5a4ab8a565cbfd381fc563bbd5
Cuddlemuffin007 <brennonkrogers@gmail.com>
2016-02-12 14:02:35 -0500

    added semi-random play logic

--- brennon_bot.py
+++ brennon_bot.py
@@ -1,4 +1,5 @@
 # Tic Tac Toe Bots - tiy extra credit *= 0
+import sys
 import random
 
 
@@ -7,12 +8,28 @@ class Game:
     def __init__(self):
         self.get_inputs()
         temp_board = [list(row) for row in self.board]
-
+        print("DEBUG temp_board", temp_board, file=sys.stderr)
+        empty = "_"
+        valid_moves = [(i, j) for j in range(3) for i in range(3) if temp_board[i][j] == empty]
+        print("DEBUG valid moves", valid_moves, file=sys.stderr)
+        locations = {
+                "center": (1, 1),
+                "corners": [(0, 0), (0, 2), (2, 0), (2, 2)],
+                "sides": [(0, 1), (1, 0), (1, 2), (2, 1)]
+            }
         move = ()
-        for row in temp_board:
-            for elem in row:
-                if elem != self.me and elem != self.you:
-                    move = (temp_board.index(row), row.index(elem))
+        if locations["center"] in valid_moves:
+            move = locations["center"]
+        else:
+            valid_move = False
+            while not valid_move:
+                move = random.choice(valid_moves)
+                if move in locations["corners"]:
+                    valid_move = True
+                elif len(valid_moves) <= 4:
+                    move = random.choice(valid_moves)
+                    valid_move = True
+
         print("{} {}".format(move[0], move[1]))
 
     def get_inputs(self):
